4840f7a2e455322c0cc8e57d75cb8f044a097138
jessicaflorencio98 <com.jflorenciom24@alumnos.cecyteg.edu.mx>
2025-04-01 16:43:33 -0600

    danna fea

--- practica 6.py
+++ practica 6.py
@@ -1,5 +1,6 @@
-nombre="jessica"
+nombre="danna fea"
 edad=16
 print("hola\n ",nombre,"\ntienes\n",edad,"\naños")
 print("python","es","genial",sep="-")
 print("hola",nombre,"tienes",edad,"años",sep="\n")
+print(" hola ",danna")
